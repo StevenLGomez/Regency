e23f059a52d074fc609e670861a65936a9840e90
Steven.Gomez <steve.gomez@biomerieux.com>
2020-02-07 16:48:02 -0600

    Modifications to ownerReport.py to start the journey of migrating from sqlite to MySQL

--- sqlite/ownerReport.py
+++ sqlite/ownerReport.py
@@ -5,48 +5,56 @@ db = sqlite3.connect('regencyEstates.sqlite')
 
 cursor = db.cursor()
 
-#cursor.execute("SELECT COUNT(id) FROM trans;")
-#transactionInfo = cursor.fetchone();
-#tCount = transactionInfo[0]
+# Enumerations for owner table columns (19 columns in original sqlite database)
 #
-#print('*******************************************************************')
-#print('Total transactions %2d ' % (tCount))
-
-#cursor.execute("SELECT date, id, amount, note FROM trans ORDER BY date")
-#cursor.execute("SELECT * FROM owner ORDER BY lot")
-#ownerQueryResult = cursor.fetchall()
-
-#for owner in ownerQueryResult:
-#    print(owner)
+#  0 id
+#  1 lot
+#  2 first
+#  3 mi
+#  4 last
+#  5 first_2
+#  6 mi_2
+#  7 last_2
+#  8 address
+#  9 city
+# 10 state
+# 11 zip
+# 12 phone
+# 13 email
+# 14 phone_2
+# 15 email_2
+# 16 buy_date
+# 17 sell_date    <<-- will not be carried forward
+# 18 is_current
 
 for lot in range(68):
-    print('*******************************************************************')
-    print('**************** Owner history for lot %2d ************************' % int(lot + 1))
+    print('-- ****************************************************************')
+    print('-- ************* Owner history for lot %2d ************************' % int(lot + 1))
+    print('-- ****************************************************************')
     cursor.execute("SELECT * FROM owner WHERE lot = :lot ORDER BY buy_date",  {"lot": lot + 1})
     ownerQueryResult = cursor.fetchall()
 
     for owner in ownerQueryResult:
-        #print(owner)
-        print('%s, %s %s  \t\t %s, %s %s' % (owner[4], owner[2], owner[3], owner[7], owner[5], owner[6]))
-        print('%s \t%s %s, %s ' % (owner[8], owner[9], owner[10], owner[11]))
-        if owner[16] == None:
-            buyDate = '--------'
-        else:
-            buyDate = owner[16]
 
-        if owner[17] == None:
-            sellDate = '--------'
-        else:
-            sellDate = owner[16]
+        #      0   1    2   3   4  5    6   7   8  9   10  11  12  13  14  15  16  18
+        print('%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s' % (owner[0], owner[1], owner[2], owner[3], owner[4], owner[5], owner[6], owner[7], owner[8], owner[9], owner[10], owner[11], owner[12], owner[13], owner[14], owner[15], owner[16], owner[18]))
+        # print('%s, %s, %s, %s, %s, %s' % (owner[4], owner[2], owner[3], owner[7], owner[5], owner[6]))
+        # print('%s \t%s %s, %s ' % (owner[8], owner[9], owner[10], owner[11]))
 
-        print('Buy Date: %s \t Sell Date: %s ' % (buyDate, sellDate))
-        print('-------------------------------------------------------------------')
+
+        # if owner[16] == None:
+        #     buyDate = '--------'
+        # else:
+        #     buyDate = owner[16]
+        # print('Buy Date: %s \t Sell Date: %s ' % (buyDate))
+
+        print('-- ----------------------------------------------------------------')
 
 
 print('')
-print('************************ End of report ****************************')
-print('*******************************************************************')
-print('*******************************************************************')
+print('-- ****************************************************************')
+print('-- ********************* End of report ****************************')
+print('-- ****************************************************************')
 
 db.close()
 
