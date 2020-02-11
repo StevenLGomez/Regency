ac7feffc2380c5bcd76cf633daef5299804a0372
Steven.Gomez <steve.gomez@biomerieux.com>
2020-02-11 17:02:26 -0600

    Modified to change from listing by lot, to just listing by ID. This will be more appropriate to migrate from sqlite to mysql.

--- sqlite/ownerReport.py
+++ sqlite/ownerReport.py
@@ -27,28 +27,43 @@ cursor = db.cursor()
 # 17 sell_date    <<-- will not be carried forward
 # 18 is_current
 
-for lot in range(68):
-    print('-- ****************************************************************')
-    print('-- ************* Owner history for lot %2d ************************' % int(lot + 1))
-    print('-- ****************************************************************')
-    cursor.execute("SELECT * FROM owner WHERE lot = :lot ORDER BY buy_date",  {"lot": lot + 1})
-    ownerQueryResult = cursor.fetchall()
+cursor.execute("SELECT COUNT(*) FROM owner;")
+entries = cursor.fetchone()
 
-    for owner in ownerQueryResult:
+# print(type(entries))
+# print(entries[0])
 
-        #      0   1    2   3   4  5    6   7   8  9   10  11  12  13  14  15  16  18
-        print('%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s' % (owner[0], owner[1], owner[2], owner[3], owner[4], owner[5], owner[6], owner[7], owner[8], owner[9], owner[10], owner[11], owner[12], owner[13], owner[14], owner[15], owner[16], owner[18]))
-        # print('%s, %s, %s, %s, %s, %s' % (owner[4], owner[2], owner[3], owner[7], owner[5], owner[6]))
-        # print('%s \t%s %s, %s ' % (owner[8], owner[9], owner[10], owner[11]))
+## for lot in range(68):
+#for lot in range(2):
+#    print('-- ****************************************************************')
+#    print('-- ************* Owner history for lot %2d ************************' % int(lot + 1))
+#    print('-- ****************************************************************')
+#    cursor.execute("SELECT * FROM owner WHERE lot = :lot ORDER BY buy_date",  {"lot": lot + 1})
+#    ownerQueryResult = cursor.fetchall()
+#
+#    for owner in ownerQueryResult:
+#
+#        #      0   1    2   3   4  5    6   7   8  9   10  11  12  13  14  15  16  18
+#        print('%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s' % (owner[0], owner[1], owner[2], owner[3], owner[4], owner[5], owner[6], owner[7], owner[8], owner[9], owner[10], owner[11], owner[12], owner[13], owner[14], owner[15], owner[16], owner[18]))
+#
+#        # print('%s, %s, %s, %s, %s, %s' % (owner[4], owner[2], owner[3], owner[7], owner[5], owner[6]))
+#        # print('%s \t%s %s, %s ' % (owner[8], owner[9], owner[10], owner[11]))
+#
+#
+#        # if owner[16] == None:
+#        #     buyDate = '--------'
+#        # else:
+#        #     buyDate = owner[16]
+#        # print('Buy Date: %s \t Sell Date: %s ' % (buyDate))
+#
+#        print('-- ----------------------------------------------------------------')
+#
 
-
-        # if owner[16] == None:
-        #     buyDate = '--------'
-        # else:
-        #     buyDate = owner[16]
-        # print('Buy Date: %s \t Sell Date: %s ' % (buyDate))
-
-        print('-- ----------------------------------------------------------------')
+cursor.execute("Select * FROM owner ORDER BY id;")
+for entry in range(entries[0]):
+    row = cursor.fetchone()
+    # print(ThisEntry)
+    print('%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s' % (row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13], row[14], row[15], row[16], row[18]))
 
 
 print('')
