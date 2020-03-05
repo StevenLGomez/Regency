ac34c1e418e5ed5467bc7870d9c8d2b9f29b7637
Steven.Gomez <steve.gomez@biomerieux.com>
2020-03-05 08:57:25 -0600

    Modified to include INSERT statement for owner table (untested)

--- sqlite/ownerReport.py
+++ sqlite/ownerReport.py
@@ -60,12 +60,27 @@ entries = cursor.fetchone()
 #        print('-- ----------------------------------------------------------------')
 #
 
-cursor.execute("Select * FROM owner ORDER BY id;")
-for entry in range(entries[0]):
-    row = cursor.fetchone()
-    # print(ThisEntry)
-    print('%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s' % (row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13], row[14], row[15], row[16], row[18]))
+# INSERT INTO owner(c1, c2, c3) 
+# VALUES
+# (v1, v2, v3),
+# (v1, v2, v3),
+# (v1, v2, v3);
 
+cursor.execute("Select * FROM owner ORDER BY id;")
+
+print('INSERT INTO owner(id, lot, first, mi, last, first_2, mi_2, last_2, address, city, state, zip, phone, email, phone_2, email_2, buy_date, is_current) VALUES')
+print('VALUES')
+
+
+# for entry in range(entries[0]):
+for entry in range(3):
+
+    row = cursor.fetchone()
+
+
+    print('(%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s),' % (row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13], row[14], row[15], row[16], row[18]))
+
+print(';')
 
 print('')
 print('-- ****************************************************************')
